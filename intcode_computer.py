77012867d8f6d93672130d4a2f969efaab158b05
Romir Moza <romir.moza@gmail.com>
2019-12-11 20:15:04 -0500

    created separate file for intcode computer class

---
+++ intcode_computer.py
@@ -0,0 +1,21 @@
+class intcode_computer:
+    
+    def __init__(self, intcode):
+        self.intcode = intcode
+
+    def alarm1202(self, noun = 12, verb = 2):
+        self.intcode[1] = noun
+        self.intcode[2] = verb
+
+    def intcode_parser(self):
+        pc = 0
+        while self.intcode[pc] != 99:
+            if self.intcode[pc] == 1:
+                self.intcode[self.intcode[pc + 3]] = self.intcode[self.intcode[pc + 1]] + self.intcode[self.intcode[pc + 2]]
+                pc += 4
+            elif self.intcode[pc] == 2:
+                self.intcode[self.intcode[pc + 3]] = self.intcode[self.intcode[pc + 1]] * self.intcode[self.intcode[pc + 2]]
+                pc += 4
+
+    def get_intcode(self):
+        return self.intcode

8c564787e53d49c80a42c0e127879a931c10797d
Romir Moza <romir.moza@gmail.com>
2019-12-11 21:28:34 -0500

    intcode_computer opcodes 3,4 added; modified class private class functions

--- intcode_computer.py
+++ intcode_computer.py
@@ -1,24 +1,67 @@
 class intcode_computer:
 
-    def __init__(self, intcode=[]):
+    def __init__(self, intcode=[], input=1):
         self.intcode = intcode
+        self.input = input
+        self.output = ""
 
-    def alarm1202(self, noun = 12, verb = 2):
+    def set_alarm1202(self, noun=12, verb=2):
         self.intcode[1] = noun
         self.intcode[2] = verb
 
     def intcode_parser(self):
         pc = 0
         while self.intcode[pc] != 99:
-            if self.intcode[pc] == 1:
-                self.intcode[self.intcode[pc + 3]] = self.intcode[self.intcode[pc + 1]] + self.intcode[self.intcode[pc + 2]]
-                pc += 4
-            elif self.intcode[pc] == 2:
-                self.intcode[self.intcode[pc + 3]] = self.intcode[self.intcode[pc + 1]] * self.intcode[self.intcode[pc + 2]]
-                pc += 4
+            [opcode, mode1, mode2, mode3] = self.__decipher_instruction(pc)
+            pc = self.__execute_instruction(pc, opcode, mode1, mode2, mode3)
+
+    def __execute_instruction(self, pc, opcode, mode1, mode2, mode3):
+        if opcode == 1:
+            param1 = self.__get_parameter(pc + 1, mode1)
+            param2 = self.__get_parameter(pc + 2, mode2)
+            self.__set_parameter(pc + 3, mode3, param1 + param2)
+            pc += 4
+        elif opcode == 2:
+            param1 = self.__get_parameter(pc + 1, mode1)
+            param2 = self.__get_parameter(pc + 2, mode2)
+            self.__set_parameter(pc + 3, mode3, param1 * param2)
+            pc += 4
+        elif opcode == 3:
+            self.__set_parameter(pc + 1, mode1, self.input)
+            pc += 2
+        elif opcode == 4:
+            param1 = self.__get_parameter(pc + 1, mode1)
+            self.output = self.output + str(param1) + " "
+            pc += 2
+        return pc
 
     def set_intcode(self, intcode):
         self.intcode = intcode
 
     def get_intcode(self):
         return self.intcode
+
+    def __get_parameter(self, pos, mode):
+        if mode:        # immediate mode
+            return self.intcode[pos]
+        return self.intcode[self.intcode[pos]]       # position mode
+
+    def __set_parameter(self, pos, mode, result):
+        if mode:        # immediate mode
+            self.intcode[pos] = result
+        self.intcode[self.intcode[pos]] = result     # position mode
+
+    def __decipher_instruction(self, pc):
+        code = self.intcode[pc]
+        opcode = code % 100
+        code = code // 100
+        mode1 = code % 10
+        code = code / 10
+        mode2 = code % 10
+        code = code / 10
+        mode3 = code % 10
+
+        return [opcode, mode1, mode2, mode3]
+
+    def get_output(self):
+        return self.output

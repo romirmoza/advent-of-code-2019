c392d4e7c79194b735033569d23e5d5212414f79
Romir Moza <romir.moza@gmail.com>
2019-12-11 20:15:44 -0500

    importing new intcode computer class

--- day2_program_alarm_a.py
+++ day2_program_alarm_a.py
@@ -1,23 +1,10 @@
-def alarm1202(intcode, noun = 12, verb = 2):
-    intcode[1] = noun
-    intcode[2] = verb
-    return 0
-
-def intcode_parser(intcode):
-    pc = 0
-    while intcode[pc] != 99:
-        if intcode[pc] == 1:
-            intcode[intcode[pc + 3]] = intcode[intcode[pc + 1]] + intcode[intcode[pc + 2]]
-            pc += 4
-        elif intcode[pc] == 2:
-            intcode[intcode[pc + 3]] = intcode[intcode[pc + 1]] * intcode[intcode[pc + 2]]
-            pc += 4
-    return 0
+from intcode_computer import intcode_computer
 
 if __name__ == '__main__':
     file = open('day2_input.txt', 'r')
     intcode = map(int, file.read().split(','))
 
-    alarm1202(intcode)
-    intcode_parser(intcode)
-    print('Output of the IntCode = {}'.format(intcode[0]))
+    computer = intcode_computer(intcode)
+    computer.alarm1202()
+    computer.intcode_parser()
+    print('Output of the IntCode = {}'.format(computer.get_intcode()[0]))

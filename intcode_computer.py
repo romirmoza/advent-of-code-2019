1ce6ceef6b3912f245e5440da90058ce0c0a636a
Romir Moza <romir.moza@gmail.com>
2019-12-12 19:12:02 -0500

    added opcode 9, relative mode and variable memory capability to the intcode computer

--- intcode_computer.py
+++ intcode_computer.py
@@ -1,11 +1,12 @@
 class intcode_computer:
 
-    def __init__(self, intcode=[], input=[1]):
+    def __init__(self, intcode=[], input=[1,1]):
         self.intcode = intcode
         self.input = input
         self.inputcounter = 0
         self.pc = 0
         self.output = ''
+        self.rel_base = 0
 
     def set_alarm1202(self, noun=12, verb=2):
         self.intcode[1] = noun
@@ -30,15 +31,15 @@ class intcode_computer:
         if opcode == 1:
             param1 = self.__get_parameter(self.pc + 1, mode1)
             param2 = self.__get_parameter(self.pc + 2, mode2)
-            self.__set_parameter(self.pc + 3, param1 + param2)
+            self.__set_parameter(self.pc + 3, mode3, param1 + param2)
             self.pc += 4
         elif opcode == 2:
             param1 = self.__get_parameter(self.pc + 1, mode1)
             param2 = self.__get_parameter(self.pc + 2, mode2)
-            self.__set_parameter(self.pc + 3, param1 * param2)
+            self.__set_parameter(self.pc + 3, mode3, param1 * param2)
             self.pc += 4
         elif opcode == 3:
-            self.__set_parameter(self.pc + 1, self.input[self.inputcounter])
+            self.__set_parameter(self.pc + 1, mode1, self.input[self.inputcounter])
             self.inputcounter += 1
             self.pc += 2
         elif opcode == 4:
@@ -57,20 +58,23 @@ class intcode_computer:
             param1 = self.__get_parameter(self.pc + 1, mode1)
             param2 = self.__get_parameter(self.pc + 2, mode2)
             if param1 < param2:
-                self.__set_parameter(self.pc + 3, 1)
+                self.__set_parameter(self.pc + 3, mode3, 1)
             else:
-                self.__set_parameter(self.pc + 3, 0)
+                self.__set_parameter(self.pc + 3, mode3, 0)
             self.pc += 4
         elif opcode == 8:
             param1 = self.__get_parameter(self.pc + 1, mode1)
             param2 = self.__get_parameter(self.pc + 2, mode2)
             param3 = self.__get_parameter(self.pc + 3, mode3)
             if param1 == param2:
-                self.__set_parameter(self.pc + 3, 1)
+                self.__set_parameter(self.pc + 3, mode3, 1)
             else:
-                self.__set_parameter(self.pc + 3, 0)
+                self.__set_parameter(self.pc + 3, mode3, 0)
             self.pc += 4
-        return self.pc
+        elif opcode == 9:
+            param1 = self.__get_parameter(self.pc + 1, mode1)
+            self.rel_base += param1
+            self.pc += 2
 
     def set_intcode(self, intcode):
         self.intcode = intcode
@@ -86,12 +90,34 @@ class intcode_computer:
         return self.intcode
 
     def __get_parameter(self, pos, mode):
-        if mode:        # immediate mode
+        if mode == 0:          # position mode
+            if self.intcode[pos] > len(self.intcode) - 1:       # if memory location is out the initial program req
+                    return 0
+            return self.intcode[self.intcode[pos]]
+        elif mode == 1:        # immediate mode
+            if pos > len(self.intcode) - 1:
+                    return 0
             return self.intcode[pos]
-        return self.intcode[self.intcode[pos]]       # position mode
+        elif mode == 2:        # relative mode
+            if self.rel_base + self.intcode[pos] > len(self.intcode) - 1:
+                    return 0
+            return self.intcode[self.rel_base + self.intcode[pos]]
 
-    def __set_parameter(self, pos, result):
-        self.intcode[self.intcode[pos]] = result     # only position mode is valid
+
+    def __set_parameter(self, pos, mode, result):
+        # only position mode is valid
+        if mode == 0:
+            if self.intcode[pos] > len(self.intcode) - 1: # requires allocating more memory
+                self.intcode = self.intcode + [0 for _ in range(self.intcode[pos] - len(self.intcode) + 1)]
+                self.intcode[self.intcode[pos]] = result
+            else:
+                self.intcode[self.intcode[pos]] = result
+        if mode == 2:
+            if self.rel_base  + self.intcode[pos] > len(self.intcode) - 1: # requires allocating more memory
+                self.intcode = self.intcode + [0 for _ in range(self.rel_base + self.intcode[pos] - len(self.intcode) + 1)]
+                self.intcode[self.rel_base  + self.intcode[pos]] = result
+            else:
+                self.intcode[self.rel_base  + self.intcode[pos]] = result
 
     def __decipher_instruction(self):
         code = self.intcode[self.pc]
